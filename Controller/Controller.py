19f275c1bf2e9e483a4aef0dd9e0ddc1a25afbcb
Lunev Dmitriy <lunev.da@phystech.edu>
2022-12-18 20:41:02 +0300

    refactor system of directories

---
+++ Controller/Controller.py
@@ -0,0 +1,49 @@
+
+
+from Simulation.Simulation import Simulation
+from Visualisator.Visualisator import Visualisator
+
+from pygame.rect import Rect
+
+class Controller(object):
+
+    def __init__(self, rect: Rect, simulation: Simulation, visualisator: Visualisator):
+        self._simulation = simulation
+        self._visualisator = visualisator
+        self._rect = rect
+
+    def change_rect(self, rect: Rect):
+        self._rect = rect
+
+    def add_body(self, mouse_start_pos, mouse_cur_pos):
+        position = (
+            (mouse_start_pos[0] - self._rect.width / 2) / self._visualisator.scale + self._visualisator.view_point[0],
+            (mouse_start_pos[1] - self._rect.height / 2) / self._visualisator.scale + self._visualisator.view_point[1]
+        )
+        mass = 10
+        radius = 10
+        velocity = (
+            (mouse_cur_pos[0] - mouse_start_pos[0]) / self._visualisator.scale,
+            (mouse_start_pos[0] - mouse_start_pos[0]) / self._visualisator.scale
+        )
+        self._simulation.add_body(
+            mass,
+            position[0],
+            position[1],
+            velocity[0],
+            velocity[1],
+            radius,
+            (255, 255, 255)
+        )
+
+    def strike(self, start_mouse_pos, cur_mouse_pos):
+        position = (
+            (start_mouse_pos[0] - self._rect.width / 2) / self._visualisator.scale + self._visualisator.view_point[0],
+            (start_mouse_pos[1] - self._rect.height / 2) / self._visualisator.scale + self._visualisator.view_point[1]
+        )
+
+        impulse = (
+            (start_mouse_pos[0] - cur_mouse_pos[0]),
+            (start_mouse_pos[1] - cur_mouse_pos[1])
+        )
+        self._simulation.strike_in_point(position, impulse)
---
+++ UI/UIElements/Mark.py
@@ -0,0 +1,31 @@
+from pygame.surface import Surface
+
+from UI.UIObject import *
+
+
+class Mark(UIObject):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius, radius, create_standard_font(), "", create_empty_icon(), (255, 0, 0))
+
+    def event_handler(self, event):
+        pass
+
+    def on_mouse_hover(self):
+        pass
+
+    def on_mouse_down(self):
+        pass
+
+    def on_mouse_enter(self):
+        pass
+
+    def on_mouse_leave(self):
+        pass
+
+    def on_mouse_up(self):
+        pass
+
+    def draw(self, surface: Surface):
+        pygame.draw.circle(surface, self._color, (self._x, self._y), self._width, self._width // 3)
+
